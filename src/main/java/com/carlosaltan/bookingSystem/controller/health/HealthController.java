eeb7268c6b6a921e528abb9a8818e39f26b3ee90
carlosaltan18 <carlosaltan850@gmail.com>
2024-09-06 20:37:56 -0600

    Commit inicial

---
+++ src/main/java/com/carlosaltan/bookingSystem/controller/health/HealthController.java
@@ -0,0 +1,15 @@
+package com.carlosaltan.bookingSystem.controller.health;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String checkAPI() {
+        return "<h1>The API is working ok!</h1>";
+    }
+}
